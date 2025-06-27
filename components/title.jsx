e98da63a5598eaddc030cb94ce9a57a3fdbad6de
LeoTiTo <lochuutran12@gmail.com>
2025-06-27 21:10:36 +0200

    refactor: remove unused import from Title component

--- components/title.jsx
+++ components/title.jsx
@@ -1,6 +1,4 @@
 // components/Title.jsx
-import { Search } from 'lucide-react'
-
 export default function Title({ title, symbol }) {
     return (
       <div className="flex items-center justify-center gap-4 mb-4 uppercase">
